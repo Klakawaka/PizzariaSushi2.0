0392d9987eba6a75b8d49c3a51c6c8aa4e949659
Klakawaka <91070430+Klakawaka@users.noreply.github.com>
2023-11-01 00:02:54 +0100

    Created menus to the webside

---
+++ PizzeriaSushi2.0/Pages/Menu.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PizzeriaSushi2._0.Pages;
+
+public class MenuModel : PageModel
+{
+    private readonly ILogger<MenuModel> _logger;
+
+    public MenuModel(ILogger<MenuModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
+

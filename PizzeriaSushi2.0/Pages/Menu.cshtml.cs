5d64d46f26e0f3f2159a1040565ba8d823d5eeac
Mush <llama0789@gmail.com>
2023-12-06 16:58:50 +0100

    Menu now fetches data from database.

--- PizzeriaSushi2.0/Pages/Menu.cshtml.cs
+++ PizzeriaSushi2.0/Pages/Menu.cshtml.cs
@@ -1,19 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using PizzeriaSushi2._0.Database;
+using PizzeriaSushi2._0.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
-namespace PizzeriaSushi2._0.Pages;
-
-public class MenuModel : PageModel
+namespace PizzeriaSushi2._0.Pages
 {
-    private readonly ILogger<MenuModel> _logger;
-
-    public MenuModel(ILogger<MenuModel> logger)
+    public class MenuModel : PageModel
     {
-        _logger = logger;
-    }
+        private readonly ILogger<MenuModel> _logger;
+        private readonly PizzeriaSushi2._0.Database.Database _context;
 
-    public void OnGet()
-    {
+        public MenuModel(ILogger<MenuModel> logger, PizzeriaSushi2._0.Database.Database context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public List<Item> MenuItems { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            MenuItems = await _context.Items.ToListAsync();
+        }
     }
 }
-
